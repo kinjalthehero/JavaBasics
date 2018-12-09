c1945d733ff24cefea50eec6f805a9f55ef94138
Kinjal Mistry <kinjalthehero@gmail.com>
2018-12-09 07:22:14 -0500

    Adding Enum folder

---
+++ src/Enum/EnumInsideClass.java
@@ -0,0 +1,17 @@
+package Enum;
+
+public class EnumInsideClass {
+
+    // enum is declared inside a class (Note enum keyword instead of class keyword)
+    enum Color {
+        RED, GREEN, BLUE;
+    }
+
+    public static void main (String[] args) {
+        // DO NOT USE new keyword while creating an object
+        // Also note the way object is defined. Format used for class is not used.
+        // This is not used -> Color c1 = Color (RED)
+        Color colorClassInsideAClass = Color.RED;
+        System.out.println(colorClassInsideAClass);
+    }
+}
---
+++ src/Enum/EnumOutsideClass.java
@@ -0,0 +1,20 @@
+package Enum;
+
+// enum is declared outside any class (Note enum keyword instead of class keyword)
+enum Color
+{
+    RED, GREEN, BLUE;
+}
+
+public class EnumOutsideClass
+{
+    // Driver method
+    public static void main(String[] args)
+    {
+        // DO NOT USE new keyword while creating an object
+        // Also note the way object is defined. Format used for class is not used.
+        // This is not used -> Color c1 = Color (RED)
+        Color c1 = Color.RED;
+        System.out.println(c1);
+    }
+}
