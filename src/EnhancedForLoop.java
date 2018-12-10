eb9ce4a91f7f429074cc1bce5bf7733d50327619
Kinjal Mistry <kmistry@akamai.com>
2018-12-10 14:20:33 -0500

    Enhanced For loop

---
+++ src/EnhancedForLoop.java
@@ -0,0 +1,17 @@
+public class EnhancedForLoop {
+
+
+
+    public static void main(String[] args) {
+
+        String[] arr = {"a",  "b", "c"};
+
+        for (String s: arr){
+            System.out.println(s);
+        }
+
+    }
+
+
+
+}
