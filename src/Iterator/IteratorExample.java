9e45fe97a7ca95e4011c8ca3c470b5cf57ac53fa
Kinjal Mistry <kinjalthehero@gmail.com>
2019-01-19 23:50:27 -0500

    Iterator v2

--- src/Iterator/IteratorExample.java
+++ src/Iterator/IteratorExample.java
@@ -7,12 +7,30 @@ public class IteratorExample {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numbers = new ArrayList<>(10);
+        ArrayList<String> games = new ArrayList<>(10);
 
-        for (int i=1; i<=10; i++)
-            numbers.add(i);
+        games.add("abc");
+        games.add("pqr");
+        games.add("ttt");
+        games.add("vvv");
 
-        Iterator<Integer> itr = numbers.iterator();
+        System.out.println("original Size of ArrayList : " + games.size());
+
+        //Looping over ArrayList in Java using advanced for loop
+        System.out.println("Looping over ArrayList in Java using advanced for loop");
+        for(String game: games){
+            //print each element from ArrayList
+            System.out.print(game + " ");
+        }
+
+        //You can also Loop over ArrayList using traditional for loop
+        System.out.println("Looping ArrayList in Java using simple for loop");
+        for(int i =0; i<games.size(); i++){
+            System.out.print(games.get(i)+ " ");
+        }
+
+        System.out.println("Looping ArrayList in Java using Iterator");
+        Iterator<String> itr = games.iterator();
         while(itr.hasNext()) {
             System.out.print(itr.next() + " ");
             //System.out.println("Removing " + itr.next() + " element from the array");
