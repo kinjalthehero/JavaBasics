fbd30d026473bee5332faecfd8d4bc9360b1384f
Kinjal Mistry <kinjalthehero@gmail.com>
2019-07-12 09:12:39 -0400

    Queue and Stack

---
+++ src/practiceProblems/stack/Stack_UsingLinkedList.java
@@ -0,0 +1,83 @@
+package practiceProblems.stack;
+
+
+public class Stack_UsingLinkedList {
+	
+	// the first node
+	private Node head; 
+ 
+	// nest class to define linkedlist node
+	private class Node {
+		int value;
+		Node next;
+	}
+ 
+	public Stack_UsingLinkedList() {
+		head = null;
+	}
+ 
+	// Remove value from the beginning of the list
+	public int pop() throws LinkedListEmptyException 
+	{
+		if (head == null) 
+		{
+			throw new LinkedListEmptyException();
+		}
+		int value = head.value;
+		head = head.next;
+		return value;
+	}
+ 
+	// Add value to the beginning of the list
+	public void push(int value) 
+	{
+		Node oldHead = head;
+		head = new Node();
+		head.value = value;
+		head.next = oldHead;
+	}
+ 
+	public static void main(String args[]) 
+	{
+		Stack_UsingLinkedList lls=new Stack_UsingLinkedList();
+		lls.push(20);
+		lls.push(50);
+		lls.push(80);
+		lls.push(40);
+		lls.push(60);
+		lls.push(75);
+		System.out.println("Element removed from LinkedList: "+lls.pop());
+		System.out.println("Element removed from LinkedList: "+lls.pop());
+		lls.push(10);
+		System.out.println("Element removed from LinkedList: "+lls.pop());
+		printList(lls.head);
+	}
+	public static void printList(Node head) 
+	{
+		Node temp = head;
+		while (temp != null) 
+		{
+			System.out.format("%d ", temp.value);
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+}
+ 
+/**
+ * 
+ * Exception to indicate that LinkedList is empty.
+ */
+ 
+class LinkedListEmptyException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+ 
+	public LinkedListEmptyException() {
+		super();
+	}
+ 
+	public LinkedListEmptyException(String message) {
+		super(message);
+	}
+}
