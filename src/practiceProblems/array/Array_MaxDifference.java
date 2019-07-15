9e13eee9b21fccaf3d06fb79db9b1b4218ff13d7
Kinjal Mistry <kinjalthehero@gmail.com>
2019-07-15 09:25:40 -0400

    Array problems

---
+++ src/practiceProblems/array/Array_MaxDifference.java
@@ -0,0 +1,35 @@
+package practiceProblems.array;
+
+public class Array_MaxDifference {
+
+    public static int calculateMaxDifferenceBetweenTwoElements(int[] arr)
+    {
+        int minElementTillNow = arr[0];
+        int maxDifference = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            int difference = 0;
+
+            if(arr[i] > minElementTillNow)
+            {
+                difference = arr[i]-minElementTillNow;
+                if(difference > maxDifference)
+                {
+                    maxDifference=difference;
+                }
+            }
+            else
+            {
+                minElementTillNow = arr[i];
+            }
+        }
+        return maxDifference;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={14, 12, 70, 15, 95, 65, 22, 30};
+        System.out.println("Maximum difference between two elements : "+calculateMaxDifferenceBetweenTwoElements(arr));
+
+    }
+}
